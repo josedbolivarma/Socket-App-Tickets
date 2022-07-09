2aca752a739c4c0860b041dd8a09344f2f8a7752
Jose David <josedbolivarmafrontend@gmail.com>
2022-07-09 13:26:42 -0500

    Project Completed

---
+++ sockets/controller.js
@@ -0,0 +1,58 @@
+const TicketControl = require('../models/ticket-control');
+
+const ticketControl = new TicketControl;
+
+const socketController = (socket) => {
+
+    socket.emit('ultimo-ticket', ticketControl.ultimo );
+    
+    socket.emit('tickets-en-cola', ticketControl.tickets.length );
+
+    socket.emit('estado-actual', ticketControl.ultimos4 );
+
+    socket.on('siguiente-ticket', ( payload, callback ) => {
+        const siguiente = ticketControl.siguiente();
+        callback( siguiente );
+        socket.broadcast.emit('tickets-en-cola', ticketControl.tickets.length );
+    });
+
+    socket.on('atender-ticket', ( { escritorio }, callback ) => {
+        
+        if ( !escritorio )  {
+            return callback({
+                ok: false,
+                msg: 'El escritorio es obligatorio'
+            });
+        };
+        
+        const ticket = ticketControl.atenderTicket( escritorio );
+
+        socket.broadcast.emit('estado-actual', ticketControl.ultimos4 );
+        
+        socket.emit('tickets-en-cola', ticketControl.tickets.length );
+        socket.broadcast.emit('tickets-en-cola', ticketControl.tickets.length );
+
+        if ( !ticket ) {
+            callback({
+                ok: false,
+                msg: 'Ya no hay tickets pendientes'
+            });
+        } else {
+            callback({
+                ok: true,
+                ticket,
+            });
+        };
+
+    })
+
+
+    
+}
+
+
+
+module.exports = {
+    socketController
+}
+
